5fc589b5abf5fe7af57d526e2f3de07a8548837c
zenoh1213 <industrialacessorios3@gmail.com>
2026-05-13 19:08:39 -0300

    Implement basic Express API for transfers

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const fetch = require('node-fetch')
+
+const app = express()
+
+app.use(express.json())
+
+app.post('/saque', async (req, res) => {
+
+  try {
+
+    const response = await fetch(
+      'https://app.sigilopay.com.br/api/v1/gateway/transfers',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-public-key': 'SUA_PUBLIC_KEY',
+          'x-secret-key': 'SUA_SECRET_KEY'
+        },
+        body: JSON.stringify(req.body)
+      }
+    )
+
+    const data = await response.json()
+
+    res.json(data)
+
+  } catch(err) {
+
+    res.status(500).json({
+      error: err.message
+    })
+
+  }
+
+})
+
+app.get('/', (req, res) => {
+  res.send('API ONLINE')
+})
+
+app.listen(process.env.PORT || 3000)
